cc2d096f4a9f9ec390dd6a5e210f13cd89308b99
Emil Kristensen <emil@ekx.dk>
2019-07-07 19:30:28 +0200

    First simple test

---
+++ tests/Feature/RegisterCloseAfterOneUserTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
+use Tests\TestCase;
+
+class RegisterCloseAfterOneUserTest extends TestCase
+{
+    // https://laravel.com/docs/5.4/database-testing#using-migrations
+    use DatabaseMigrations;
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testWithNoUser()
+    {
+        $response = $this->get('/register');
+
+        $response->assertStatus(200);
+    }
+
+    public function testWithUserRegistered()
+    {
+        $user = new User();
+        $user->password = Hash::make('password');
+        $user->email = "example@example.org";
+        $user->name = "John Doe";
+        $user->save();
+
+        $response = $this->get('/register');
+
+        $response->assertStatus(302);
+    }
+}
