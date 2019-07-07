e4ebe2a484fd6da440ca72615b19685c743a7c72
Emil Kristensen <emil@ekx.dk>
2019-07-07 23:34:14 +0200

    Test old purchages are not shown on refund page

--- tests/Feature/ViewBeerRefundTest.php
+++ tests/Feature/ViewBeerRefundTest.php
@@ -58,4 +58,43 @@ class ViewBeerRefundTest extends TestCase
         $response->assertSee("-".($product->price*2).".00 kr.");
         $response->assertSee($product->name);
     }
+
+
+    public function testDontShowOldPurchase()
+    {
+        $room = Room::create([
+            'id' => 1,
+            'name' => 'Test room'
+        ]);
+
+        $product = Product::create([
+            'name' => 'Test product',
+            'color' => 'fff',
+            'quantity' => '1,2,5',
+            'price' => '1232.00',
+        ]);
+
+        $quantity = 2;
+
+        $beer = new Beer();
+        $beer->room = $room->id;
+        $beer->quantity = $quantity;
+        $beer->product = $product->id;
+        $beer->ipAddress = request()->ip();
+        $beer->amount = -($product->price * $quantity);
+        $beer->save();
+
+        // Set time to be 35 minutes behind in time for all purchases
+        \DB::update(
+            'UPDATE beers SET created_at = ?',
+            [\Carbon\Carbon::now()->subMinutes(35)->toDateTimeString()]
+        );
+
+        $response = $this->get('/refund');
+
+        $response->assertStatus(200);
+
+        $response->assertDontSee("-".($product->price*2).".00 kr.");
+        $response->assertDontSee($product->name);
+    }
 }
