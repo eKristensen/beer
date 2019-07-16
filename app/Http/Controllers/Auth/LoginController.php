477a1c3ac82702ddca97336acb55572ba94b84f1
Emil Kristensen <eKristensen@users.noreply.github.com>
2019-07-16 19:36:57 +0200

    Use middleware to redirect

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -21,13 +21,6 @@ class LoginController extends Controller
     use AuthenticatesUsers;
 
     /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/rooms/edit';
-
-    /**
      * Max login attempts.
      *
      * @var int
