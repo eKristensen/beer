54ffbbfb8aa11ea281402bc0b2ac8ae932f3d35b
Emil Kristensen <e@ekristensen.dk>
2019-07-27 14:24:35 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/HighScoreController.php
+++ app/Http/Controllers/HighScoreController.php
@@ -2,11 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class HighScoreController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         return view('highscore.index');
     }
 }
