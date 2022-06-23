b28588240489bbce67fd041ce1e6fbef6abd0917
Emil Kristensen <eKristensen@users.noreply.github.com>
2022-06-23 18:24:12 +0200

    Maybe upgraded to Laravel 9
    Signed-off-by: Emil Kristensen <eKristensen@users.noreply.github.com>

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -1,8 +1,64 @@
 <?php
 
+namespace App\Exceptions;
+
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Throwable;
+
+class Handler extends ExceptionHandler
+{
+    /**
+     * A list of exception types with their corresponding custom log levels.
+     *
+     * @var array<class-string<\Throwable>, \Psr\Log\LogLevel::*>
+     */
+    protected $levels = [
+        //
+    ];
+
+    /**
+     * A list of the exception types that are not reported.
+     *
+     * @var array<int, class-string<\Throwable>>
+     */
+    protected $dontReport = [
+        //
+    ];
+
+    /**
+     * A list of the inputs that are never flashed to the session on validation exceptions.
+     *
+     * @var array<int, string>
+     */
+    protected $dontFlash = [
+        'current_password',
+        'password',
+        'password_confirmation',
+    ];
+
+    /**
+     * Register the exception handling callbacks for the application.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->reportable(function (Throwable $e) {
+            //
+        });
+    }
+}
+[ek@ek-xps Exceptions]$ cd ..
+[ek@ek-xps app]$ cd Http/Middleware/
+[ek@ek-xps Middleware]$ ls
+Authenticate.php    PreventRequestsDuringMaintenance.php  TrimStrings.php  TrustProxies.php
+EncryptCookies.php  RedirectIfAuthenticated.php           TrustHosts.php   VerifyCsrfToken.php
+[ek@ek-xps Middleware]$ cat TrustProxies.php 
+<?php
+
 namespace App\Http\Middleware;
 
-use Fideloper\Proxy\TrustProxies as Middleware;
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
 use Illuminate\Http\Request;
 
 class TrustProxies extends Middleware
@@ -10,14 +66,19 @@ class TrustProxies extends Middleware
     /**
      * The trusted proxies for this application.
      *
-     * @var array
+     * @var array<int, string>|string|null
      */
     protected $proxies;
 
     /**
      * The headers that should be used to detect proxies.
      *
-     * @var string
+     * @var int
      */
-    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+    protected $headers =
+        Request::HEADER_X_FORWARDED_FOR |
+        Request::HEADER_X_FORWARDED_HOST |
+        Request::HEADER_X_FORWARDED_PORT |
+        Request::HEADER_X_FORWARDED_PROTO |
+        Request::HEADER_X_FORWARDED_AWS_ELB;
 }
